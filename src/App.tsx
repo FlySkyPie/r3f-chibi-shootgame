64b7b5bf411f94e67019d541cf32bfbd0a4129d4
Wei Ji(FlySkyPie) <c445dj544@gmail.com>
2023-01-11 21:03:22 +0800

    feat: Add r3f sample

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useRef, useState } from 'react'
+import { Canvas, MeshProps, useFrame } from '@react-three/fiber'
+
+function Box(props: MeshProps) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef<any>();
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += delta))
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  )
+}
+
+export default App

887b857c942438dbdc4f34aca4c1646099b13832
Wei Ji(FlySkyPie) <c445dj544@gmail.com>
2023-01-14 09:34:16 +0800

    feat: Change character asset

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useMemo, useRef, useState } from 'react'
 import { Canvas, MeshProps, useFrame } from '@react-three/fiber'
-import { Grid, MapControls, OrbitControls } from '@react-three/drei';
+import { CycleRaycast, Grid, MapControls, OrbitControls, Plane } from '@react-three/drei';
 
 import { ChibiCharacter } from './components/ChibiCharacter';
 import { DragonBonesTicker } from './components/DragonBonesTicker';
@@ -52,8 +52,18 @@ function App() {
         <meshStandardMaterial color={'orange'} />
       </mesh> */}
 
+      <mesh
+        rotation={[-Math.PI * 0.5, 0, 0]}
+        onPointerOver={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <planeGeometry args={[2000, 2000]} />
+        <meshStandardMaterial color={0xcdc6b1} />
+      </mesh>
+
       <Grid
-        position={[0, 0, 0]}
+        position={[0, 0.05, 0]}
         args={[100, 100]}
         cellSize={10}
         cellThickness={1.5}
@@ -68,6 +78,11 @@ function App() {
         infiniteGrid
       />
       <DragonBonesTicker />
+      {/* <CycleRaycast onChanged={(objects, cycle) => {
+        console.log(objects, cycle);
+        return null;
+      }} /> */}
+      
     </Canvas >
   )
 }
